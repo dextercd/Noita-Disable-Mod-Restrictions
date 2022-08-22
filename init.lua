ca3ef001bf405cdaa479c247fcf4c8d64fcd8b55
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2022-08-22 17:06:50 +0200

    Disable more mod checks

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
 -- Seeker of knowledge? :^)
 
+local mod_id = "disable-mod-restrictions"
+function setting_get(name)
+    return ModSettingGet(mod_id .. "." .. name)
+end
+
 local ffi = require("ffi")
 
 ffi.cdef([[
@@ -9,44 +14,192 @@ int memcmp(const void *buffer1, const void *buffer2, size_t count);
 
 ]])
 
-local err_msg = "Could not alter Noita.  Records will not be stored since you're using mods."
-local suc_msg = "Success! Records will be kept despite having mods active."
+local nop2 = ffi.new("char[2]", {0x66, 0x90})
+local nop6 = ffi.new("char[6]", {0x66, 0x0f, 0x1f, 0x44, 0x00, 0x00})
 
-function abort_patch(message)
-    GamePrint(message)
-    GamePrint(err_msg)
-    error(message)
+local test_non_zero2 = ffi.new("char[2]", {0x85, 0xe4})
+
+local game_modifications = {
+    records = {
+        {
+            location = ffi.cast("void*", 0x009c1b00),
+            original = ffi.new("char[2]", {0x77, 0x1b}),
+            patch_bytes = ffi.new("char[2]", {0xeb, 0x2c}),
+        },
+    },
+
+    achievements = {
+        { -- By name
+            location = ffi.cast("void*", 0x0076d1f2),
+            original = ffi.new("char[2]", {0xa1, 0x60}),
+            patch_bytes = ffi.new("char[2]", {0xeb, 0x1f}),
+        },
+        { -- By ID
+            location = ffi.cast("void*", 0x0076d292),
+            original = ffi.new("char[2]", {0xa1, 0x60}),
+            patch_bytes = ffi.new("char[2]", {0xeb, 0x1f}),
+        },
+    },
+
+    cauldron = {
+        {
+            location = ffi.cast("void*", 0x005c84e7),
+            original = ffi.new("char[6]", {0x0f, 0x85, 0x41, 0x07, 0x00, 0x00}),
+            patch_bytes = nop6,
+        },
+    },
+
+    eyes = {
+        {
+            location = ffi.cast("void*", 0x005b4185),
+            original = ffi.new("char[6]", {0x0f, 0x85, 0x9d, 0x01, 0x00, 0x00}),
+            patch_bytes = nop6,
+        },
+    },
+
+    bones = {
+        {
+            -- Crash if wands have custom spells?
+            location = ffi.cast("void*", 0x006416b4),
+            original = ffi.new("char[6]", {0x0f, 0x87, 0x29, 0x01, 0x00, 0x00}),
+            patch_bytes = nop6,
+        }
+    },
+
+    streaks = {
+        {
+            location = ffi.cast("void*", 0x0063a4ac),
+            original = ffi.new("char[2]", {0x74, 0x25}),
+            patch_bytes = nop2,
+        },
+        {
+            location = ffi.cast("void*", 0x006750ee),
+            original = ffi.new("char[2]", {0x84, 0xc0}),
+            patch_bytes = test_non_zero2,
+        },
+        {
+            location = ffi.cast("void*", 0x009c1110),
+            original = ffi.new("char[2]", {0x84, 0xc0}),
+            patch_bytes = test_non_zero2,
+        },
+        {
+            location = ffi.cast("void*", 0x009c1127),
+            original = ffi.new("char[2]", {0x84, 0xc0}),
+            patch_bytes = test_non_zero2,
+        },
+        {
+            location = ffi.cast("void*", 0x009c1abc),
+            original = ffi.new("char[2]", {0x84, 0xc0}),
+            patch_bytes = test_non_zero2,
+        },
+    },
+
+    gods_are_very_curious = {
+        {
+            location = ffi.cast("void*", 0x00aa4337),
+            original = ffi.new("char[6]", {0x0f, 0x85, 0x77, 0x01, 0x00, 0x00}),
+            patch_bytes = nop6,
+        },
+    },
+}
+
+
+-- The init.lua script will get unloaded & reloaded when you start a new game so
+-- we need some code to keep track of which patches have been enabled.  Just do
+-- so by looking at the memory.
+
+function get_enabled_modifications()
+    local enabled = {}
+    for name, modification in pairs(game_modifications) do
+        local patch = modification[1]
+        enabled[name] = ffi.C.memcmp(patch.location, patch.patch_bytes, ffi.sizeof(patch.patch_bytes)) == 0
+    end
+    return enabled
 end
 
-local target_location = ffi.cast("void*", 0x9c1b00)
-local expected = ffi.new("char[2]", {0x77, 0x1b})
-local replacement = ffi.new("char[2]", {0xeb, 0x2c})
 
-if ffi.C.memcmp(target_location, replacement, ffi.sizeof(replacement)) == 0 then
-    -- Already patched
-    GamePrint(suc_msg)
-    return
+-- Desired modifications are the values that were configured by the user.
+
+function get_desired_modifications()
+    local desired = {}
+    for name, _ in pairs(game_modifications) do
+        desired[name] = setting_get(name)
+    end
+    return desired
 end
 
-if ffi.C.memcmp(target_location, expected, ffi.sizeof(expected)) ~= 0 then
-    msg = "Incorrect instructions at target location. " ..
-          "This mod only works on noita.exe, Apr 23 2021, Steam version!"
-    abort_patch(msg)
+
+-- Compare enabled vs. desired and reconfigure to desired.
+
+function configure_desired_modifications()
+    local enabled = get_enabled_modifications()
+    local desired = get_desired_modifications()
+
+    for name, modification in pairs(game_modifications) do
+        if enabled[name] ~= desired[name] then
+            configure_modification(modification, desired[name])
+        end
+    end
+
+    local enabled = get_enabled_modifications()
+    local desired = get_desired_modifications()
+
+    for name, modification in pairs(game_modifications) do
+        if enabled[name] ~= desired[name] then
+            print(
+                "Warning: modification " .. name ..
+                " couldn't be configured to desired state: " ..
+                tostring(desired[name])
+            )
+        end
+    end
 end
 
-local restore_protection = ffi.new("int[1]")
-local prot_success = ffi.C.VirtualProtect(
-    target_location, ffi.sizeof(replacement), 0x40, restore_protection)
 
-if not prot_success then
-    abort_patch("Couldn't change memory protection.")
+-- Might've changed settings in the pause menu, reconfigure the patches.
+
+function OnPausedChanged()
+    configure_desired_modifications()
 end
 
-ffi.copy(target_location, replacement, ffi.sizeof(replacement))
-ffi.C.VirtualProtect(
-    target_location,
-    ffi.sizeof(replacement),
-    restore_protection[0],
-    restore_protection)
 
-GamePrint(suc_msg)
+function configure_modification(modification, enable)
+    for _, patch in ipairs(modification) do
+        local patch_bytes = enable and patch.patch_bytes or patch.original
+        local expect =      enable and patch.original or patch.patch_bytes
+
+        patch_location(patch.location, expect, patch_bytes)
+    end
+end
+
+function patch_location(location, expect, patch_bytes)
+    if ffi.C.memcmp(location, expect, ffi.sizeof(expect)) ~= 0 then
+        print("Unexpected instructions at location.")
+        return false
+    end
+
+    local restore_protection = ffi.new("int[1]")
+    local prot_success = ffi.C.VirtualProtect(
+        location, ffi.sizeof(patch_bytes), 0x40, restore_protection)
+
+    if not prot_success then
+        print("Couldn't change memory protection.")
+        return false
+    end
+
+    ffi.copy(location, patch_bytes, ffi.sizeof(patch_bytes))
+
+    -- Restore protection
+    ffi.C.VirtualProtect(
+        location,
+        ffi.sizeof(patch_bytes),
+        restore_protection[0],
+        restore_protection)
+
+    return true
+end
+
+
+-- Initial configuration when you start a run.
+
+configure_desired_modifications()
---
+++ settings.lua
@@ -0,0 +1,147 @@
+dofile("data/scripts/lib/mod_settings.lua")
+
+local mod_id = "disable-mod-restrictions"
+
+local modifications = {
+    {
+        key = "records",
+        name = "Records",
+        description = "Track new gameplay records such as time, gold, max hp, etc..",
+    },
+    {
+        key = "streaks",
+        name = "Track streaks",
+        description = "Track win streaks and display them on the game over screen.",
+    },
+
+    {
+        key = "achievements",
+        name = "Steam Achievements",
+        description = "Get Steam Achievements despite having mods active.",
+        --[[
+        You cheated not only the game, but yourself.
+
+        You didn't grow.
+        You didn't improve.
+        You took a shortcut and gained nothing.
+
+        You experienced a hollow victory.
+        Nothing was risked and nothing was gained.
+
+        It's sad that you don't know the difference.
+
+        lol ]]
+    },
+
+    {
+        key = "bones",
+        name = "Create new bones files. (new wands for Ghosts/Apparitions)",
+        description = "Allow bones files to be created while having mods active.",
+        disclaimer = "This will cause ghosts to spawn with incomplete wands if you later disable mods that add spells.",
+    },
+
+    {
+        key = "gods_are_very_curious",
+        name = "Negative health message",
+        description = "Enables the 'The Gods are very curious' message when your health is below zero.",
+    },
+
+    {
+        key = "cauldron",
+        name = "Cauldron secret",
+        description = "Allows the Cauldron to spawn into the world.",
+        spoiler = true,
+    },
+    {
+        key = "eyes",
+        name = "Eyes secret",
+        description = "Re-enable the encrypted eye messages.",
+        spoiler = true,
+    },
+}
+
+mod_settings = {}
+
+function populate_mod_settings(reveal_spoilers)
+    function spoiler_name(name)
+        return name:sub(1, 1) .. string.rep("x", #name - 1)
+    end
+
+    function spoiler_description(desc)
+        return "Reveal spoilers for more details."
+    end
+
+    function mod_settings_entry(attributes)
+        local spoiler = (attributes.spoiler and not reveal_spoilers)
+        local name = spoiler and spoiler_name(attributes.name) or attributes.name
+        local description = spoiler and spoiler_description(attributes.description) or attributes.description
+
+        return {
+            id = attributes.key,
+            ui_name = name,
+            ui_description = description,
+            value_default = true,
+            scope = MOD_SETTING_SCOPE_RUNTIME,
+        }
+    end
+
+    function mod_disclaimer_entry(attributes)
+        local disclaimer = spoiler and spoiler_description(attributes.disclaimer) or attributes.disclaimer
+        return {
+            id = "_",
+            ui_name = "  Warning!",
+            ui_description = attributes.disclaimer,
+            not_setting = true,
+            ui_fn = function(mod_id, gui, in_main_menu, im_id, setting)
+                GuiColorSetForNextWidget(gui, 1.0, 0.4, 0.4, 1.0)
+                GuiText(gui, mod_setting_group_x_offset, 0, setting.ui_name)
+                mod_setting_tooltip(mod_id, gui, in_main_menu, setting)
+            end
+        }
+    end
+
+    mod_settings = {}
+    spoilers_group = {
+        category_id = "spoilered_modifications",
+        ui_name = "Spoilers",
+        ui_description = "These settings may contain spoilers",
+        settings = {
+            {
+                id = "reveal_spoilers",
+                ui_name = "Reveal spoilers",
+                ui_description = "",
+                ui_fn = function(mod_id, gui, in_main_menu, im_id, setting)
+                    if GuiButton(gui, im_id, mod_setting_group_x_offset, 0, "[Reveal spoilers]") then
+                        populate_mod_settings(true)
+                    end
+                end,
+                not_setting = true,
+            },
+        }
+    }
+
+    for _, attributes in ipairs(modifications) do
+        local active_group = attributes.spoiler and spoilers_group.settings or mod_settings
+        table.insert(active_group, mod_settings_entry(attributes))
+
+        if attributes.disclaimer then
+            table.insert(active_group, mod_disclaimer_entry(attributes))
+        end
+    end
+
+    table.insert(mod_settings, spoilers_group)
+end
+
+function ModSettingsUpdate(init_scope)
+    mod_settings_update(mod_id, mod_settings, init_scope)
+end
+
+function ModSettingsGuiCount()
+    return mod_settings_gui_count(mod_id, mod_settings)
+end
+
+function ModSettingsGui(gui, in_main_menu)
+    mod_settings_gui(mod_id, mod_settings, gui, in_main_menu)
+end
+
+populate_mod_settings(false)
